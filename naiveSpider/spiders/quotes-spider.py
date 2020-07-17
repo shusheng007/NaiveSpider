e930f9ca832e43ac820c13373d6954d40a6e6f85
Ben.Wang <Ben.Wang@homecreditcfc.cn>
2020-07-17 17:44:48 +0800

    and user agent

--- naiveSpider/spiders/quotes-spider.py
+++ naiveSpider/spiders/quotes-spider.py
@@ -5,13 +5,16 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     start_urls = [
-        'https://jinqiangua.911cha.com/111111.html',
-        'https://jinqiangua.911cha.com/000000.html'
+        'https://jinqiangua.911cha.com/',
     ]
+
+    def start_requests(self):
+        headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+        for url in self.start_urls:
+            yield scrapy.Request(url,headers=headers)
 
     def parse(self, response):
         page = response.url.split("/")[-1]
         filename = 'gua-%s' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
-        self.log('saved file %' % filename)
