402a60db594fcf19006f9e237c9c58d9fd922ed5
Ben.Wang <Ben.Wang@homecreditcfc.cn>
2020-07-17 18:00:41 +0800

    user agent

--- naiveSpider/spiders/quotes-spider.py
+++ naiveSpider/spiders/quotes-spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import random
 
 
 class QuotesSpider(scrapy.Spider):
@@ -8,8 +9,21 @@ class QuotesSpider(scrapy.Spider):
         'https://jinqiangua.911cha.com/',
     ]
 
+
+     my_headers=["Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
+     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36",
+     "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0"
+     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14",
+     "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)"
+     'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'
+     ]
+	
+
+
+
     def start_requests(self):
-        headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+
+        headers= {'User-Agent': random.choice(headers) }
         for url in self.start_urls:
             yield scrapy.Request(url,headers=headers)
 
